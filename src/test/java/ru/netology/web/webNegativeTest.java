74706e07428310be948a48c4d0e1e64715d388c5
Vladimir Semenov <osmyswait@gmail.com>
2023-02-10 14:58:00 +0300

    add test for second task

---
+++ src/test/java/ru/netology/web/webNegativeTest.java
@@ -0,0 +1,112 @@
+package ru.netology.web;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class webNegativeTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setup() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    public void afterEach() {
+        driver.quit();
+    }
+
+    @Test
+    void testEnglishName() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Vladimir Semenov");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79373714484");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testHieroglyphicsInsteadName() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("!#%");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79373714484");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testWithoutName() {
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("8937371448423");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testIncorrectNumber() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Владимир-Дмитрий Семенов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+7937371448423");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testNumberWithoutPlus() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Владимир Семенов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("8937371448423");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testWithoutNumber() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Владимир Семенов");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+   @Test
+    void testCheckBoxEmpty() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Владимир Семенов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79373714484");
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid")).isDisplayed());
+    }
+}
--- src/test/java/ru/netology/web/webPositiveTest.java
+++ src/test/java/ru/netology/web/webPositiveTest.java
@@ -25,10 +25,10 @@ public class webPositiveTest {
     @BeforeEach
     public void beforeEach() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
+//        driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
 
